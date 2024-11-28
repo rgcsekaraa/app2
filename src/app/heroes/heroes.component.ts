b62bf632650e75ef412f168cb909e16ddf517ee4
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-11-28 12:57:12 +1000

    lesson 3 - done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf } from '@angular/common';
+
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
-  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
+  imports: [NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
